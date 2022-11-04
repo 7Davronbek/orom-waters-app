f2a8bac3186a58dcc33e9dc210269849695f962e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-04 15:02:12 +0500

    orom-waters-app

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,3 +1,3 @@
-export const API_PATH = 'https://backend.oromwater.uz/main'
-
 export const LANGUAGE = 'OROM_LANGUAGE'
+
+export const API_PATH = `https://backend.oromwater.uz/${localStorage.getItem(LANGUAGE) ? localStorage.getItem(LANGUAGE) : 'ru'}/main`
